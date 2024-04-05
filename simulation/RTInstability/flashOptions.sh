76eaff2421e0cb0314e54ca6afcdce15fdce29a0
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-04-04 20:13:09 -0500

    checkpoint RTwork

--- simulation/RTInstability/flashOptions.sh
+++ simulation/RTInstability/flashOptions.sh
@@ -1,6 +1,6 @@
 # cache the value of current working directory
 
-FlashSha="3cdc71bf"
+FlashSha="5aa7289e"
 
 FlashOptions="incompFlow/RTInstability -auto -maxblocks=100 -2d -nxb=16 -nyb=16 +amrex +parallelIO -site=$SiteHome +incomp"
 
